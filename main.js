310bb91b8cfda61a6fe4aa04822285802d4cb6cd
itsjoesullivan <itsjoesullivan@gmail.com>
2015-11-28 11:20:07 -0500

    remove demo files

--- main.js
+++
@@ -1,8 +0,0 @@
-var context = new AudioContext();
-var snare = require('./index');
-document.getElementById('snare').addEventListener('click', function(e) {
-  snareNode = snare(context);
-  snareNode.connect(context.destination);
-  snareNode.start(context.currentTime + 0.01);
-  snareNode.gain.value = 0.1;
-});
