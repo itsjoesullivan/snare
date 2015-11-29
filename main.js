ccdb326bf43c5a36679f298ebbd29437be47c297
itsjoesullivan <itsjoesullivan@gmail.com>
2015-11-29 12:36:15 -0500

    bring in new api

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+var Snare = require('./index');
 var context = new AudioContext();
-var snare = require('./index');
+var snare = Snare(context);
+
 document.getElementById('snare').addEventListener('click', function(e) {
-  snareNode = snare(context);
+  snareNode = snare();
   snareNode.connect(context.destination);
   snareNode.start(context.currentTime + 0.01);
-  snareNode.gain.value = 0.1;
+  snareNode.gain.value = 0.5;
 });
