7d88124184a8b9c7abaeb4d6ca454e01f6693796
Joe Sullivan <itsjoesullivan@gmail.com>
2016-05-16 16:14:45 -0400

    Move re-used graph elements to constructor

--- index.js
+++ index.js
@@ -8,40 +8,48 @@ module.exports = function(context, parameters) {
 
   var noiseBuffer = NoiseBuffer(1);
 
+  var masterHighBump = context.createBiquadFilter();
+  masterHighBump.frequency.value = 4000;
+  masterHighBump.gain.value = 6;
+  masterHighBump.type = "peaking";
+  var masterLowBump = context.createBiquadFilter();
+  masterLowBump.frequency.value = 200;
+  masterLowBump.gain.value = 12;
+  masterLowBump.type = "peaking";
+  masterHighBump.connect(masterLowBump);
+
+
+  var masterBus = context.createGain();
+  masterBus.gain.value = 0.4;
+  masterBus.connect(masterHighBump);
+
+
+  var noiseHighpass = context.createBiquadFilter();
+  noiseHighpass.type = "highpass";
+  noiseHighpass.frequency.value = 1200;
+  noiseHighpass.connect(masterBus);
+
+
+  var oscsHighpass = context.createBiquadFilter();
+  oscsHighpass.type = "highpass";
+  oscsHighpass.frequency.value = 400;
+  oscsHighpass.connect(masterBus);
+
+
   return function() {
     var audioNode = context.createGain();
-    var masterBus = context.createGain();
-    masterBus.gain.value = 0.4;
-    var masterHighBump = context.createBiquadFilter();
-    masterHighBump.type = "peaking";
-    var masterLowBump = context.createBiquadFilter();
-    masterLowBump.type = "peaking";
-    masterBus.connect(masterHighBump);
-    masterHighBump.connect(masterLowBump);
     masterLowBump.connect(audioNode);
-    masterHighBump.frequency.value = 4000;
-    masterLowBump.frequency.value = 200;
-    masterHighBump.gain.value = 6;
-    masterLowBump.gain.value = 12;
 
     var noise = context.createBufferSource();
     noise.buffer = noiseBuffer;
     noise.loop = true;
 
     var noiseGain = context.createGain();
-    var noiseHighpass = context.createBiquadFilter();
-    noiseHighpass.type = "highpass";
     noise.connect(noiseGain);
     noiseGain.connect(noiseHighpass);
-    noiseHighpass.connect(masterBus);
-    noiseHighpass.frequency.value = 1200;
 
     var oscsGain = context.createGain();
-    var oscsHighpass = context.createBiquadFilter();
     oscsGain.connect(oscsHighpass);
-    oscsHighpass.type = "highpass";
-    oscsHighpass.frequency.value = 400;
-    oscsHighpass.connect(masterBus);
 
     var oscs = [87.307, 329.628].map(function(frequency) {
       var osc = context.createOscillator();
@@ -58,18 +66,24 @@ module.exports = function(context, parameters) {
       noiseGain.gain.setValueAtTime(0.00001, when);
       noiseGain.gain.exponentialRampToValueAtTime(Math.max(0.000001, parameters.snappy / 127), when + 0.01);
       noiseGain.gain.exponentialRampToValueAtTime(0.00001, when + 0.3);
-      noise.start(when);
+      noiseGain.gain.setValueAtTime(0, when + 0.31);
 
       oscsGain.gain.setValueAtTime(0.00001, when);
       oscsGain.gain.exponentialRampToValueAtTime(1, when + 0.01);
       oscsGain.gain.exponentialRampToValueAtTime(0.00001, when + 0.2);
+      oscsGain.gain.setValueAtTime(0, when + 0.21);
 
       oscs.forEach(function(osc) {
         osc.start(when);
+        osc.stop(when + 0.3);
       });
+      var offset = Math.random() * noise.buffer.duration;
+      offset = 0;
+      noise.start(when, offset);
+      noise.stop(when + 0.3);
+
     };
     audioNode.stop = function(when) {
-      noise.stop(when);
     };
     return audioNode;
   };
